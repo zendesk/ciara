deb1fe7f600c70c5257868a4bc1cccc82f807f84
bcolfer <bcolfer@zendesk.com>
2017-09-22 15:45:11 -0700

    Moved some functionality to its own class

--- app/ciara.rb
+++ app/ciara.rb
@@ -1,23 +1,23 @@
 require "google/cloud/pubsub"
 require "json"
+require_relative "lib/../app/lib/pubsub_conneciton"
 
 class Ciara
   attr_reader :pubsub,
               :topic_name,
-              :gcloud_project,
-              :gcloud_keyfile,
               :subscription_name,
               :clair_server
 
   def initialize (gcloud_project:nil, gcloud_keyfile:nil, topic_name:nil, subscription_name:nil,  clair_server:nil)
     @topic_name = topic_name || ENV['TOPIC_NAME'] || default_topic_name
-    @gcloud_project = gcloud_project || ENV['GCLOUD_PROJECT'] || default_gcloud_project
-    @gcloud_keyfile = gcloud_keyfile || ENV['GCLOUD_KEYFILE'] || default_gcloud_keyfile
+
 
     @subscription_name = subscription_name || ENV['SUBSCRIPTION_NAME'] || default_subscription_name
     @clair_server = clair_server || ENV['CLAIR'] || default_clair_server
 
-    @pubsub = Google::Cloud::Pubsub.new(project: @gcloud_project, keyfile: @gcloud_keyfile)
+    gcloud_connection = PubsubConnection.new(gcloud_project, gcloud_keyfile)
+
+    @pubsub = gcloud_connection.pubsub
 
   end
 
---
+++ spec/get_pubsub_spec.rb
@@ -0,0 +1,22 @@
+require "rspec"
+
+module PubsubConnection
+  describe PubsubConnection do
+
+    context "the keyfile can be found" do
+      let(:keyfile) {
+
+      }
+    end
+    context "with a valid connection" do
+      let(:pubsub_connection) {PubsubConnection.new}
+
+    end
+
+    context "with a invalid connection" do
+      let(:gcloud_project) {}
+    end
+
+  end
+end
+
